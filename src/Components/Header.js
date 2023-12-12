c8c68f2821041c4154fe8426af120589a3633aa6
Triyaambak <triyaambaks2003@gmail.com>
2023-12-12 20:51:44 +0530

    12/12/23

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header () {
+    return (
+        <div className="header">
+            <img src="./Images/Troll_Face.png" alt="" className="header-logo" ></img>
+            <p className="header-title">Meme Generator</p>
+        </div>
+    )
+}
+
